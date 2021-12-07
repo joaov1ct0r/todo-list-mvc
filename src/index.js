3dd0d8d69df3c8c55cf9c5953222fef6a06bb06f
Joao Victor <jov9051@outlook.com>
2021-12-07 16:00:48 -0300

    created route /:index with method get returning one object on todolist

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ app.get("/", (req, res) => {
     res.send(JSON.stringify(toDoList));
 });
 
+// INSERI UM NOVO OBJETO NO TODOLIST
 app.post("/new", bodyParser.json(), (req, res) => {
     let id = generateId();
 
@@ -30,6 +31,13 @@ app.post("/new", bodyParser.json(), (req, res) => {
     toDoList.push({ id, title });
 
     res.send("Post adicionado com sucesso");
+});
+
+// RETORNA UM OBJETO EM ESPECIFICO NO TODOLIST
+app.get("/:index", bodyParser.json(), (req, res) => {
+    let { index } = req.params;
+
+    res.send(JSON.stringify(toDoList[index]));
 });
 
 app.listen(PORT, () => {
