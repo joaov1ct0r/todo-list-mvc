ba01ab43dca6fdc3e2af383f38e11da16afb9b7b
Joao Victor <jov9051@outlook.com>
2022-02-19 15:49:39 -0300

    funcao validateEditToDo validando dados em validateData.js

--- src/controllers/validateData.js
+++ src/controllers/validateData.js
@@ -8,4 +8,10 @@ let validateInsertToDo = data => {
     return schema.validate(data);
 };
 
-let validateEditToDo = data => {};
+let validateEditToDo = data => {
+    let schema = Joi.object({
+        title: Joi.string().required().min(2).max(50)
+    });
+
+    return schema.validate(data);
+};
