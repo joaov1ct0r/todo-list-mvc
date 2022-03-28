5d6ff5f1df43ff95899763b6fc8551a285597bb2
Joao Victor <jov9051@outlook.com>
2022-03-28 16:07:53 -0300

    model definodo em userModel.js

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -2,6 +2,26 @@ import { DataTypes } from 'sequelize';
 
 import dbConnection from '../config/database.js';
 
+const toDo = dbConnection.define(
+    'toDo',
+    {
+        toDo: {
+            type: DataTypes.STRING(60),
+            allowNull: false
+        },
+        toDoID: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        }
+    },
+    {
+        freezeTableName: true,
+        tableName: 'toDo',
+        timestamps: false
+    }
+);
+
 // RETORNA TODOS OS TO-DO LIST
 let allToDo = callback => {
     let SQL = `SELECT * FROM toDo`;
