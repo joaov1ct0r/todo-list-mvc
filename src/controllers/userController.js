ffeb0d26de778b5c16363a92972ac2b0d096be26
Joao Victor <jov9051@outlook.com>
2022-03-28 16:38:20 -0300

    async adicionado a func em userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,11 +2,11 @@ import toDo from '../model/userModel.js';
 
 import { validateInsertToDo, validateEditToDo } from './validateData';
 
-const allToDo = (req, res) => {
+const allToDo = async (req, res) => {
     try {
-        const toDos = toDo.findAll();
-        
-        if(!toDos) return res.status(400).send('Falha ao enviar toDos');
+        const toDos = await toDo.findAll();
+
+        if (!toDos) return res.status(400).send('Falha ao enviar toDos');
 
         res.send(JSON.stringify(toDos));
     } catch (error) {
@@ -14,7 +14,7 @@ const allToDo = (req, res) => {
     }
 };
 
-const insertToDo = (req, res) => {
+const insertToDo = async (req, res) => {
     let { error } = validateInsertToDo(req.body);
 
     if (error) return res.status(400).send('Falha na autenticação');
@@ -22,17 +22,18 @@ const insertToDo = (req, res) => {
     let { title } = req.body;
 
     try {
-        const newTodo = await toDo.create({toDo: title})
+        const newTodo = await toDo.create({ toDo: title });
 
-        if(!newTodo) return res.status(400).send('Falha ao adicionar novo Post')
-    
-        res.send('Post adicionado com sucesso') 
+        if (!newTodo)
+            return res.status(400).send('Falha ao adicionar novo Post');
+
+        res.send('Post adicionado com sucesso');
     } catch (error) {
-       throw error; 
+        throw error;
     }
 };
 
-const editToDo = (req, res) => {
+const editToDo = async (req, res) => {
     let { error } = validateEditToDo(req.body);
 
     if (error) return res.status(400).send('Falha na autenticação');
@@ -42,21 +43,24 @@ const editToDo = (req, res) => {
     let { index } = req.params;
 
     try {
-        const updateToDo = toDo.update({toDo: title}, {
-            where: {
-                toDoID: index
+        const updateToDo = await toDo.update(
+            { toDo: title },
+            {
+                where: {
+                    toDoID: index
+                }
             }
-        })
-    
-        if(!updateToDo) return res.status(400).send('Falha ao editar post');
-    
-        res.send('Post editado com sucesso')
+        );
+
+        if (!updateToDo) return res.status(400).send('Falha ao editar post');
+
+        res.send('Post editado com sucesso');
     } catch (error) {
-        throw error
+        throw error;
     }
 };
 
-const deleteToDo = (req, res) => {
+const deleteToDo = async (req, res) => {
     let { index } = req.params;
 
     try {
@@ -64,13 +68,13 @@ const deleteToDo = (req, res) => {
             where: {
                 toDoID: index
             }
-        })
-    
-        if(!deleteToDo) return res.status(400).send('Falha ao deletar Post')
-    
-        res.send('Post deletado com sucesso')  
+        });
+
+        if (!deleteToDo) return res.status(400).send('Falha ao deletar Post');
+
+        res.send('Post deletado com sucesso');
     } catch (error) {
-        throw error
+        throw error;
     }
 };
 
