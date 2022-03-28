00505c05c4791125c7bdada1110df0f5cda5feec
Joao Victor <jov9051@outlook.com>
2022-03-28 15:58:24 -0300

    codigos de conexao a db movido para /config/database.js

--- src/model/db.js
+++ src/model/db.js
@@ -1,24 +1,3 @@
-import sequelize from 'sequelize';
-
-let { Sequelize } = sequelize;
-
-const dbConnection = new Sequelize({
-    dialect: 'mysql',
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT
-});
-
-try {
-    await dbConnection.authenticate();
-
-    console.log('Conectado ao DB!');
-} catch (error) {
-    throw error;
-}
-
 // RETORNA TODOS OS TO-DO LIST
 let allToDo = callback => {
     let SQL = `SELECT * FROM toDo`;
