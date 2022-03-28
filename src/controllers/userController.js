aec5c8eb42115e640452b6fc112edc9d12f0e8c2
Joao Victor <jov9051@outlook.com>
2022-03-28 15:39:06 -0300

    es6+ inserido em userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,55 +1,53 @@
-const db = require('../model/db');
+import db from '../model/db.js';
 
-let { validateInsertToDo, validateEditToDo } = require('./validateData');
+import { validateInsertToDo, validateEditToDo } from './validateData';
 
-const user = {
-    allToDo(req, res) {
-        db.allToDo(function (result) {
-            res.send(JSON.stringify(result));
-        });
-    },
-
-    insertToDo(req, res) {
-        let { error } = validateInsertToDo(req.body);
-
-        if (error) {
-            return res.status(400).send('Falha na autenticação');
-        }
-        let { title } = req.body;
-
-        db.insertToDo(title, function (result) {
-            console.log(result);
-
-            res.send('Post adicionado com sucesso');
-        });
-    },
-
-    editToDo(req, res) {
-        let { error } = validateEditToDo(req.body);
-
-        if (error) {
-            return res.status(400).send('Falha na autenticação');
-        }
-        let { title } = req.body;
-
-        let { index } = req.params;
-
-        db.editToDo(title, index, function (result) {
-            console.log(result);
-
-            res.send('Post editado com sucesso');
-        });
-    },
-
-    deleteToDo(req, res) {
-        let { index } = req.params;
-
-        db.deleteToDo(index, function (result) {
-            console.log(result);
-
-            res.send('Post deletado com sucesso');
-        });
-    }
+const allToDo = (req, res) => {
+    db.allToDo(function (result) {
+        res.send(JSON.stringify(result));
+    });
 };
 
-module.exports = user;
+const insertToDo = (req, res) => {
+    let { error } = validateInsertToDo(req.body);
+
+    if (error) {
+        return res.status(400).send('Falha na autenticação');
+    }
+    let { title } = req.body;
+
+    db.insertToDo(title, function (result) {
+        console.log(result);
+
+        res.send('Post adicionado com sucesso');
+    });
+};
+
+const editToDo = (req, res) => {
+    let { error } = validateEditToDo(req.body);
+
+    if (error) {
+        return res.status(400).send('Falha na autenticação');
+    }
+    let { title } = req.body;
+
+    let { index } = req.params;
+
+    db.editToDo(title, index, function (result) {
+        console.log(result);
+
+        res.send('Post editado com sucesso');
+    });
+};
+
+const deleteToDo = (req, res) => {
+    let { index } = req.params;
+
+    db.deleteToDo(index, function (result) {
+        console.log(result);
+
+        res.send('Post deletado com sucesso');
+    });
+};
+
+export { allToDo, insertToDo, editToDo, deleteToDo };
